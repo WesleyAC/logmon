8132dc4e624374c1c44fd1eb51c17d9a24f24bbc
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-11-26 17:03:41 -0500

    Add command line options for alert window

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from datetime import datetime, timedelta
 from httplog import HttpLog
 from httpstats import HttpStats
 
-def main(filename, time_window, update_interval):
+def main(filename, time_window, update_interval, alert_window, alert_threshold):
+    #TODO(Wesley) use alert_window and alert_threshold
     input_file = open(filename, "r")
     stats = HttpStats(time_window)
     last_update = datetime(1,1,1) #TODO(Wesley) this is a hack
@@ -31,6 +32,8 @@ if __name__ == "__main__":
     parser.add_argument('file', type=str, help='what file to read from')
     parser.add_argument('--time-window', type=int, default=0, help='what time window to view stats over (seconds, default=∞)')
     parser.add_argument('--update-interval', type=int, default=10, help='how often to update the screen (seconds, default=10)')
+    parser.add_argument('--alert-window', type=int, default=2, help='what time window to use for high traffic alerts (minutes, default=2)')
+    parser.add_argument('--alert-threshold', type=int, default=500, help='how many pageviews need to occur in the alert_window to trigger an alert (default=500)')
 
     args = parser.parse_args()
-    main(args.file, args.time_window, args.update_interval)
+    main(args.file, args.time_window, args.update_interval, args.alert_window, args.alert_threshold)
